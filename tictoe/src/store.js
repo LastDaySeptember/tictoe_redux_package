c0c4d0ae07582e9ff36c6ae067aef5bb5e6c734d
LastDaySeptember <alia.stikova@gmail.com>
2025-12-07 12:57:17 +0100

    Add provider

--- tictoe/src/store.js
+++ tictoe/src/store.js
@@ -1,18 +1,4 @@
 import { reducer } from "./reducer";
 import { createStore } from "redux";
 
-// const createStore = () => {
-//   let state;
-//   return {
-//     dispatch: (action) => {
-//       state = reducer(state, action);
-//     },
-//     getState: () => {
-//       return StaticRange;
-//     },
-//   };
-// };
-
-// export const store = createStore();
-
 export const store = createStore(reducer);
